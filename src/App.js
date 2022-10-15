9b0a7fa66e2c43c1cce73e14eff363e5b68bc34d
SallyXue-0114 <92959551+SallyXue-0114@users.noreply.github.com>
2022-10-15 17:51:38 -0500

    Popup window + center text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { render } from "@testing-library/react";
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 import image from "./worldmap.png"; 
+import Popup from "./components/Popup"
 
 
-class App extends Component{
-  render(){
+function App(){
+    const [buttonPopup, setButtonPopup] = useState(false);
+    
     const myStyle = {
       backgroundImage:`url(${image})`,
       backgroundRepeat:"no-repeat",
@@ -12,13 +14,26 @@ class App extends Component{
       width: '100vw', height: '100vh', 
       backgroundPosition: 'center',
     };
+    
     return (
-      <div style = {myStyle}>
-        <h1>See major rivers in the world!</h1>
+      <div className="App">
+
+        <div style = {myStyle}>
+          <div style = {{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+            <h1>See major rivers in the world!</h1>
+            <button onClick={() => setButtonPopup(true)}>Open Popup
+            </button>
+          </div>
+        </div>
+
+        <Popup trigger = {buttonPopup} setTrigger={setButtonPopup}>
+          <h3>My popup</h3>
+          <p>information here</p>
+        </Popup>
       </div>
     );
-  }
 }
+
 
 export default App;
 
