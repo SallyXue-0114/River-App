5525073f36f27699cea89b2b5227d409d79493b2
Yuxin <76422684+Lyxxx2003@users.noreply.github.com>
2022-10-15 18:57:43 -0500

    Sample codes for data descriptions

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
-import { render } from "@testing-library/react";
-import React, {Component, useState} from "react";
-import image from "./worldmap.png"; 
-import Popup from "./components/Popup"
+import React from 'react';
+import './App.css';
 
-
-function App(){
-    const [buttonPopup, setButtonPopup] = useState(false);
-    
-    const myStyle = {
-      backgroundImage:`url(${image})`,
-      backgroundRepeat:"no-repeat",
-      backgroundSize:"contain", 
-      width: '100vw', height: '100vh', 
-      backgroundPosition: 'center',
-    };
-    
-    return (
-      <div className="App">
-
-        <div style = {myStyle}>
-          <div style = {{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-            <h1>See major rivers in the world!</h1>
-            <button onClick={() => setButtonPopup(true)}>Open Popup
-            </button>
+function App() {
+  return (
+    <div>
+      <section class = "section">
+        <div class = "box-main">
+          <div class = "firstHalf">
+            <h1 class = "text-big">
+              Boto
+            </h1>
+            <p class = "text-small">
+              Desriptions:
+            </p>
+            <p class = "text-small">
+              1. Largest river dolphin
+            </p>
+            <p class = "text-small">
+              2. Gray when young
+            </p>
+            <p class = "text-small">
+              3. Pink and white when mature
+            </p>
+              <img src = "/Desktop/UIUC/Clubs/WCS/Code ada/data/Boto.png"></img>
           </div>
         </div>
-
-        <Popup trigger = {buttonPopup} setTrigger={setButtonPopup}>
-          <h3>My popup</h3>
-          <p>information here</p>
-        </Popup>
-      </div>
-    );
+      </section>
+    </div>
+  )
 }
 
-
 export default App;
-
