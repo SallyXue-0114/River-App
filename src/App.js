6aee7f1a67a1a4ee915ff9d8bb5a0bdead028503
SallyXue-0114 <92959551+SallyXue-0114@users.noreply.github.com>
2022-10-15 19:29:46 -0500

    successfully input 6 rivers' popups into correct place

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
-import React from 'react';
-import './App.css';
+import { render } from "@testing-library/react";
+import React, {Component, useState, TouchableOpacity, Text} from "react";
+import image from "./worldmap.png"; 
+import Popup from "./components/Popup"
 
-function App() {
-  return (
-    <div>
-      <section class = "section">
-        <div class = "box-main">
-          <div class = "firstHalf">
-            <h1 class = "text-big">
-              Boto
-            </h1>
-            <p class = "text-small">
-              Desriptions:
-            </p>
-            <p class = "text-small">
-              1. Largest river dolphin
-            </p>
-            <p class = "text-small">
-              2. Gray when young
-            </p>
-            <p class = "text-small">
-              3. Pink and white when mature
-            </p>
-              <img src = "/Desktop/UIUC/Clubs/WCS/Code ada/data/Boto.png"></img>
+
+function App(){
+    const [buttonPopup, setButtonPopup] = useState(false);
+    
+    const myStyle = {
+      backgroundImage:`url(${image})`,
+      backgroundRepeat:"no-repeat",
+      backgroundSize:"contain", 
+      width: '100vw', height: '100vh', 
+      backgroundPosition: 'center',
+    };
+
+    return (
+      <div className="App">
+
+        <div style = {myStyle}>
+          <div style = {{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+            <h1>See major rivers in the world!</h1>
+            
+            <button style = {{position:"fixed", marginLeft: "65rem", marginBottom:"30rem"}} onClick={() => setButtonPopup(true)}> 
+            Yenisei River</button>
+
+            <button style = {{position:"fixed", marginLeft: "85rem", marginBottom:"-6rem"}} onClick={() => setButtonPopup(true)}> 
+            Yangtze River</button>
+
+            <button style = {{position:"fixed", marginLeft: "38rem", marginBottom:"-8rem"}} onClick={() => setButtonPopup(true)}> 
+            Nile River</button>
+
+            <button style = {{position:"fixed", marginLeft: "85rem", marginBottom:"-3rem"}} onClick={() => setButtonPopup(true)}> 
+            Yellow River</button>
+
+            <button style = {{position:"fixed", marginLeft: "-5rem", marginBottom:"-25rem"}} onClick={() => setButtonPopup(true)}> 
+            Amazon River</button>
+
+            <button style = {{position:"fixed", marginLeft: "-30rem", marginBottom:"-3rem"}} onClick={() => setButtonPopup(true)}> 
+            Mississippi River</button>
+
+          
+
           </div>
         </div>
-      </section>
-    </div>
-  )
+
+        <Popup trigger = {buttonPopup} setTrigger={setButtonPopup}>
+          <h3>Documentation</h3>
+          <p>information</p>
+        </Popup>
+      </div>
+    );
 }
 
+
 export default App;
+
